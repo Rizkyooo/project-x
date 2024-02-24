8076a1816971e914b76802210218ee73e81f4e61
Rizkyooo <ainurrizkyooo@gmail.com>
2024-02-24 11:31:10 +0800

    update hero and card

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -16,7 +16,7 @@ import {
       <Card className="w-full max-w-[26rem] shadow-2xl mb-4">
         <CardHeader floated={false} color="blue-gray">
           <img className="h-60 w-full object-cover"
-            src="https://images.unsplash.com/photo-1499696010180-025ef6e1a8f9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+            src="/images/desain01.png"
             alt="ui/ux review check"
           />
         </CardHeader>
